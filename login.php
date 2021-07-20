b3230a4fa271c6954039d7b7c668776038f9c0d5
Mudimba Moonde <mudimbamoonde@gmail.com>
2021-07-20 15:54:55 +0200

    New Owner

--- login.php
+++ login.php
@@ -30,7 +30,9 @@ if (isset($_SESSION["id"])){
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 			<div class="login-panel panel panel-default">
-				<div class="panel-heading">Log in</div>
+            <img src='coat.png' class='img-thumbnail' width='100' height='80' style="margin-left:240px;">
+            <h1 class="text-center">School Payment System</h1>
+				<div class="panel-heading text-center">Log in</div>
 				<div class="panel-body">
                     <?php
                     if (isset($_POST["submit"])){
--- schpay.sql
+++ schpay.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jul 04, 2019 at 10:39 AM
--- Server version: 5.7.26
--- PHP Version: 7.2.18
+-- Host: 127.0.0.1
+-- Generation Time: Jul 20, 2021 at 03:54 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,20 +27,18 @@ SET time_zone = "+00:00";
 -- Table structure for table `payment`
 --
 
-DROP TABLE IF EXISTS `payment`;
-CREATE TABLE IF NOT EXISTS `payment` (
-  `paymentID` int(50) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `payment` (
+  `paymentID` int(50) NOT NULL,
   `studentID` varchar(150) NOT NULL,
   `amountPaid` varchar(100) DEFAULT NULL,
   `term` int(10) NOT NULL,
   `paymentType` varchar(100) NOT NULL,
-  `receiptBatchNumber` text,
-  `description` text,
-  `attachment` text,
+  `receiptBatchNumber` text DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `attachment` text DEFAULT NULL,
   `dateOfPayments` date NOT NULL,
-  `statusApproval` varchar(11) NOT NULL,
-  PRIMARY KEY (`paymentID`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `statusApproval` varchar(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `payment`
@@ -51,7 +48,8 @@ INSERT INTO `payment` (`paymentID`, `studentID`, `amountPaid`, `term`, `paymentT
 (1, '1', '1200', 1, 'School Fee', '29890710857', 'Registrations, meal', 'How To Make An Ethernet Cable - Simple Instructions.pdf', '2019-07-04', 'rejected'),
 (2, '1', '9201', 2, 'School Fee', '23173691873', 'Registration', 'Candidate Registration Schedule Combined (SSA)_Interactive_PDF.pdf', '2019-07-04', 'pending'),
 (3, '2', '1200', 1, 'School Fee', '23173691873', 'Registration, meals', 'Candidate Registration Schedule Combined (SSA)_Interactive_PDF.pdf', '2019-07-04', 'approved'),
-(4, '1', '200', 1, 'Exams', '131308301', 'Exams', 'The Python Standard Library â€” Python 3.7.1 documentation.pdf', '2019-07-04', 'pending');
+(4, '1', '200', 1, 'Exams', '131308301', 'Exams', 'The Python Standard Library ??\" Python 3.7.1 documentation.pdf', '2019-07-04', 'pending'),
+(5, '1', '3133', 1, 'School Fee', '4448557849', 'sc', 'v.PNG', '2021-06-21', 'pending');
 
 -- --------------------------------------------------------
 
@@ -59,25 +57,23 @@ INSERT INTO `payment` (`paymentID`, `studentID`, `amountPaid`, `term`, `paymentT
 -- Table structure for table `student`
 --
 
-DROP TABLE IF EXISTS `student`;
-CREATE TABLE IF NOT EXISTS `student` (
-  `sn` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `student` (
+  `sn` int(10) NOT NULL,
   `firstName` varchar(100) NOT NULL,
   `surname` varchar(100) NOT NULL,
   `dob` date DEFAULT NULL,
   `grade` enum('1','2','3','4','5','6','7','8','9','10','11','12') DEFAULT NULL,
   `class` varchar(100) NOT NULL,
-  `address` text,
-  `account` int(11) NOT NULL,
-  PRIMARY KEY (`sn`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `address` text DEFAULT NULL,
+  `account` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `student`
 --
 
 INSERT INTO `student` (`sn`, `firstName`, `surname`, `dob`, `grade`, `class`, `address`, `account`) VALUES
-(1, 'Mudimba', 'Headson', '2019-06-25', '10', 'A', 'MKUSHI', 1),
+(1, 'Linda', 'Mwenesha', '2019-06-25', '10', 'A', 'MKUSHI', 1),
 (2, 'Moonga', 'kelvin', '2019-06-25', '10', 'A', 'MKUSHI', 1),
 (3, 'Linda', 'Kamuti', '1966-12-22', '11', 'C', 'Kalingalinga, kamploops road lusaka', 0);
 
@@ -87,9 +83,8 @@ INSERT INTO `student` (`sn`, `firstName`, `surname`, `dob`, `grade`, `class`, `a
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `ID` int(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `ID` int(20) NOT NULL,
   `firstName` varchar(100) NOT NULL,
   `surname` varchar(100) NOT NULL,
   `email` varchar(200) NOT NULL,
@@ -97,18 +92,61 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(200) NOT NULL,
   `accountLevel` varchar(100) DEFAULT NULL,
   `username` varchar(100) DEFAULT NULL,
-  `status` int(10) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `status` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`ID`, `firstName`, `surname`, `email`, `mobile`, `password`, `accountLevel`, `username`, `status`) VALUES
-(1, 'Mudimba', 'Headson', 'n/a', 0, '41b7fa1956f021a54f3c290ec60e469c', 'student', '1', 1),
-(2, 'Nyondo', 'Maria', 'nyondo@gmail.com', 39121, 'ce679f7b3f30a53a18e7cead77c983c9', 'Admin', 'nyodo', 1),
-(4, 'Moonga', 'kelvin', 'n/a', 0, '7fde97cf4cf355af302cf9e90cfd701a', 'student', '2', 1);
+(1, 'linda', 'Mwenesha', 'n/a', 0, '4880ef2a0d55a5e89538c4150b3a9bae', 'student', '1', 1),
+(2, 'Bridget', 'Mwenesha', 'brigetm@gmail.com', 39121, '4880ef2a0d55a5e89538c4150b3a9bae', 'Admin', 'admin', 1),
+(4, 'Moonga', 'kelvin', 'n/a', 0, '4880ef2a0d55a5e89538c4150b3a9bae', 'student', '2', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`paymentID`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`sn`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `paymentID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `sn` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
